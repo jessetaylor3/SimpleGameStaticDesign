99dfabcd42a62c50c33f8992f62957f82103d4d6
Seth_Dijkstra <119450434+SethDijkstra@users.noreply.github.com>
2023-10-31 16:18:13 -0600

    Added Audio

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-function navigateTo(screenName){
-    switch(screenName) {
-        case 'Settings':
-            window.location.href = 'settings.html';
-            break;
-        case 'GameScreen':
-            window.location.href = 'gameScreen.html';
-            break;
+function navigateTo(screen) {
+    switch (screen) {
         case 'LockerRoom':
             window.location.href = 'lockerRoom.html';
             break;
+        case 'GameScreen':
+            let audioElement = document.getElementById('backgroundAudio');
+
+            // Play the audio and then navigate to GameScreen
+            audioElement.oncanplaythrough = function() {
+                audioElement.play();
+                window.location.href = 'gameScreen.html';
+            };
+            audioElement.load(); // Trigger loading of the audio
+            break;
+        case 'Settings':
+            window.location.href = 'settings.html';
+            break;
         default:
-            console.error("Unknown screen: " + screenName);
+            console.error(`Unknown screen: ${screen}`);
+            break;
     }
 }
+
 
 document.addEventListener('DOMContentLoaded', function() {
     let iconWrappers = document.querySelectorAll('.iconWrapper');
@@ -26,6 +35,25 @@ document.addEventListener('DOMContentLoaded', function() {
             // Redirect to the Game Screen with a query parameter
             window.location.href = `gameScreen.html?plane=${planeName}`;
         });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    let audioElement = document.getElementById('backgroundAudio');
+    let startGameBtn = document.getElementById('startGameBtn');
+
+    startGameBtn.addEventListener('click', function() {
+        try {
+            audioElement.play().catch(error => {
+                // Log for debugging purposes
+                console.error("Audio playback error:", error);
+                
+                // If there's an error, display an alert or some kind of message to the user
+                alert("Audio could not be played. Please try again.");
+            });
+        } catch (error) {
+            console.error("Audio playback error:", error);
+        }
     });
 });
 
